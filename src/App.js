b54374eb8a05eb9d6b048388d70e7fe0dbcdeec1
Hamza <hamza.rafi@ug.bilkent.edu.tr>
2021-03-21 18:23:27 +0500

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { FaTimes } from 'react-icons/fa'
+
 
 function App() {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    fetchComments();
+  }, [])
+
+  const fetchComments = () => {
+    Axios.get('http://localhost:8000/comment').then((response) => {
+      //console.log(response.data);
+      setComments(response.data);
+    })
+  }
+
+  const deleteComment = (id) => {
+    Axios.delete(`http://localhost:8000/comment/${id}`).then((resp) => {
+      console.log(resp);  
+      setComments(resp.data.result);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="title">Comments Page</h2>
+      <div>
+        <ul>
+          <li> id</li>
+          <li>Comment</li>
+          <li>postid</li>
+               <li>To delete</li>  
+              </ul>    
+      {comments.map((val) =>{
+        return <ul>
+                <li>{val.id}</li>
+                <li>{val.body}</li>
+                <li>{val.postId}</li>
+                <li><FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => {
+                    console.log(`clicked ${val.id}`)
+                    deleteComment(val.id)
+                  }}/></li>  
+              </ul>    
+      })}
+      
+         
+      </div>
     </div>
   );
 }
